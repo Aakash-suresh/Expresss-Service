782030e92ad5ad6eae108c6691a20544e986ff2e
Aakash-suresh <MY_NAME@example.comaakashakshith455@gmail.com>
2023-04-25 10:49:29 +0530

    Developed without using ID

--- servicePage.js
+++ servicePage.js
@@ -1,72 +1,52 @@
 const express = require('express')
 const router = express.Router();
 
-const listOfService = [
-    { id: 1, Service: "Web Development", description: "Build high-quality web applications using modern technologies" },
-    { id: 2, Service: "Mobile App Development", description: "Develop mobile applications for iOS and Android platforms" },
-    { id: 3, Service: "Backend Development", description: "Design and implement scalable and secure backend systems" },
-    { id: 4, Service: "Frontend Development", description: "Create dynamic and responsive user interfaces using popular frameworks" },
-    { id: 5, Service: "Data Engineering", description: "Extract, transform, and load large datasets to support business operations" },
-    { id: 6, Service: "AI Development", description: "Build intelligent systems using machine learning and other AI techniques" },
-    { id: 7, Service: "Research and Development", description: "Explore emerging technologies and innovate new solutions" },
-    { id: 8, Service: "Cloud Architecture", description: "Design and deploy scalable and reliable cloud infrastructure" }
-  ];
-
 router.get('/',(request,response)=>{
     response.send("This is Service Page...")
 })
 
-// router.get('/:id',(request,response)=>{
-//     if(request.params.id==1)
-//     {
-//         response.send("The Service Number is "+request.params.id+"And the service is Web development")
-//     }
-//     else if(request.params.id==2)
-//     {
-//         response.send("The Service Number is "+request.params.id+"And the service is MobileApp Development")
-//     }
-//     else if(request.params.id==3)
-//     {
-//         response.send("The Service Number is "+request.params.id+"And the service is Backend Development")
-//     }
-//     else if(request.params.id==4)
-//     {
-//         response.send("The Service Number is "+request.params.id+"And the service is Frontend Development")
-//     }
-//     else if(request.params.id==5)
-//     {
-//         response.send("The Service Number is "+request.params.id+"And the service is Data Engineer")
-//     }
-//     else if(request.params.id==6)
-//     {
-//         response.send("The Service Number is "+request.params.id+"And the service is AI Development")
-//     }
-//     else if(request.params.id==7)
-//     {
-//         response.send("The Service Number is "+request.params.id+"And the service is Research And Development")
-//     }
-//     else if(request.params.id==8)
-//     {
-//         response.send("The Service Number is "+request.params.id+"And the service is Cloud Architect")
-//     }
-//     else
-//     {
-//         response.send("Service not Found...")
-//     }  
-// })
-
-router.get('/:id',(request,response)=>{
-    const serviceId = Number(request.params.id)
-    const getService = listOfService.find((service)=>service.id=== serviceId)
-    if(!getService)
-    {
-        response.status(500).send("Expected Service Not Found...")
-        console.log(request.params.id)
-    }
-    else
-    {
-        response.json(getService)
-    }
+router.get('/:id',(request,response) =>{
+    // response.write(`id is ${request.params.id}\n`)
+    response.write(`S.No : ${request.user.S_NO}\n`)
+    response.write(`Service : ${request.user.Service}\n`)
+    response.end(`Description : ${request.user.Service}`)
 })
 
-module.exports=router 
+router.param('id', (request,response, next, id) =>{
+     request.user = listOfService[id - 1]
+     next()
+})
+
+const listOfService = [
+    { S_NO: 1, Service: "Web Development", description: "Build high-quality web applications using modern technologies" },
+    { S_NO: 2, Service: "Mobile App Development", description: "Develop mobile applications for iOS and Android platforms" },
+    { S_NO: 3, Service: "Backend Development", description: "Design and implement scalable and secure backend systems" },
+    { S_NO: 4, Service: "Frontend Development", description: "Create dynamic and responsive user interfaces using popular frameworks" },
+    { S_NO: 5, Service: "Data Engineering", description: "Extract, transform, and load large datasets to support business operations" },
+    { S_NO: 6, Service: "AI Development", description: "Build intelligent systems using machine learning and other AI techniques" },
+    { S_NO: 7, Service: "Research and Development", description: "Explore emerging technologies and innovate new solutions" },
+    { S_NO: 8, Service: "Cloud Architecture", description: "Design and deploy scalable and reliable cloud infrastructure" }
+];
+
+router.route('/:id')
+    .get((request,response)=>{  
+        const serviceId = Number(request.params.id)
+        const getService = listOfService.find((service)=>service.S_NO === serviceId)
+        if(!getService)
+        {
+            response.status(404).send("Service not Found...")
+        }
+        else
+        {
+            request.user = getService
+            response.send(`id is ${request.params.id}\nS.NO : ${request.user.S_NO}\nService : ${request.user.Service}\nDescription : ${request.user.description}`)
+        }
+    })
+    .put((request,response) =>{
+      response.send(`id is ${request.params.id}`)
+    })
+    .delete((request,response)=>{
+        response.send(`Delete method on user page with id ${request.params.id}`)
+    })
+
+module.exports=router
